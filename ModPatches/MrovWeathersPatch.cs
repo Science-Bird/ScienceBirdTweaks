259b1450c1aee48b09e8bde6fcd712da3dfa535e
xameryn <maximilianramann@gmail.com>
2025-04-04 04:07:12 -0700

    TrueBlackoutPatch Improvements
    - >10x speedup
    - Triggered on calling Mrov's Blackout.OnEnable() instead of listening for changes
    - Handles more material types / lights
    - Lights no longer found/iterated twice as it replaces Blackout.OnEnable()
    - Skips removing lighting from some non-blacklisted objects
    - Added timer logging for profiling
    
    Recycled some code from Mrov 🫡

--- ModPatches/MrovWeathersPatch.cs
+++ ModPatches/MrovWeathersPatch.cs
@@ -7,7 +7,12 @@ using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Linq;
 using BepInEx;
-using UnityEngine.UIElements;
+using System.Diagnostics;
+using System.Text;
+using static UnityEngine.UI.Image;
+using System.Collections;
+using UnityEngine.Rendering.HighDefinition;
+using MrovWeathers;
 
 
 namespace ScienceBirdTweaks.ModPatches
@@ -16,184 +21,235 @@ namespace ScienceBirdTweaks.ModPatches
     {
         public static void DoPatching()
         {
-            MethodInfo method1 = typeof(WeatherEffectController).GetMethods().FirstOrDefault(x => x.Name == "SetWeatherEffects" && x.GetParameters().FirstOrDefault()?.ParameterType == typeof(Weather[]));
-            MethodInfo method2 = typeof(WeatherEffectController).GetMethods().FirstOrDefault(x => x.Name == "SetWeatherEffects" && x.GetParameters().FirstOrDefault()?.ParameterType == typeof(LevelWeatherType[]));
-            MethodInfo method3 = typeof(WeatherEffectController).GetMethods().FirstOrDefault(x => x.Name == "SetWeatherEffects" && x.GetParameters().FirstOrDefault()?.ParameterType == typeof(Weather));
-
-            ScienceBirdTweaks.Harmony?.Patch(method1, postfix: new HarmonyMethod(typeof(TrueBlackoutPatch).GetMethod("OnSetWeathers")));
-            ScienceBirdTweaks.Harmony?.Patch(method2, postfix: new HarmonyMethod(typeof(TrueBlackoutPatch).GetMethod("OnSetWeatherTypes")));
-            ScienceBirdTweaks.Harmony?.Patch(method3, postfix: new HarmonyMethod(typeof(TrueBlackoutPatch).GetMethod("OnSetWeather")));
+            System.Type blackoutType = AccessTools.TypeByName("MrovWeathers.Blackout");
+            if (blackoutType != null)
+            {
+                MethodInfo onEnable = AccessTools.Method(blackoutType, "OnEnable");
+                MethodInfo customMethod = typeof(TrueBlackoutPatch).GetMethod(nameof(TrueBlackoutPatch.BlackoutOverridePrefix), BindingFlags.Static | BindingFlags.Public);
+                if (onEnable != null && customMethod != null)
+                {
+                    ScienceBirdTweaks.Harmony.Patch(onEnable, prefix: new HarmonyMethod(customMethod));
+                }
+            }
         }
     }
 
-    public class TrueBlackoutPatch
+    public static class TrueBlackoutPatch
     {
-        public static void BlackoutOverride()
+        private static readonly int emissionColorID = Shader.PropertyToID("_EmissionColor");
+        private static readonly int emissiveColorID = Shader.PropertyToID("_EmissiveColor");
+        private static readonly int useEmissiveIntensityID = Shader.PropertyToID("_UseEmissiveIntensity");
+        private static readonly int emissionMapID = Shader.PropertyToID("_EmissionMap");
+        private static readonly int surfaceTypeID = Shader.PropertyToID("_SurfaceType");
+        private const string emissiveColorMapKeyword = "_EMISSIVE_COLOR_MAP";
+
+        public static bool BlackoutOverridePrefix(object __instance)
         {
+            BlackoutOverride();
+
+            return false;
+        }
+
+        private static void BlackoutOverride()
+        {
+            var totalStopwatch = Stopwatch.StartNew();
+            var extraLogs = false;
+
             ScienceBirdTweaks.Logger.LogInfo("Doing blackout override...");
             Scene scene = SceneManager.GetActiveScene();
+
             if (!scene.isLoaded)
             {
                 ScienceBirdTweaks.Logger.LogWarning($"Scene is not loaded! Aborting!");
+                totalStopwatch.Stop();
+                ScienceBirdTweaks.Logger.LogDebug($"[TIMER] TOTAL BlackoutOverride execution time (aborted): {totalStopwatch.ElapsedMilliseconds}ms");
                 return;
             }
 
-            List<GameObject> lightObjects = new List<GameObject>();
-            GameObject[] objectList = Resources.FindObjectsOfTypeAll<GameObject>().ToArray();
+            ScienceBirdTweaks.Logger.LogDebug($"[TIMER] Light component search: {totalStopwatch.ElapsedMilliseconds}ms");
 
-            List<GameObject> lightObjectBlacklist = new List<GameObject>();
+            //Light[] allLights = GameObject.FindObjectsOfType<Light>(true); // My method
+            List<Light> allLights = Blackout.LightUtils.GetLightsInScene(StartOfRound.Instance.currentLevel.sceneName); // Mrov's method
 
-            foreach (GameObject obj in objectList)
+            //List<Light> allLights = new List<Light>(); // Merge of the two
+            //allLights.AddRange(GameObject.FindObjectsOfType<Light>(true));
+            //allLights.AddRange(Blackout.LightUtils.GetLightsInScene(StartOfRound.Instance.currentLevel.sceneName));
+            //allLights = allLights.Distinct().ToList();
+
+            HashSet<GameObject> lightObjects = new HashSet<GameObject>();
+            HashSet<GameObject> lightObjectsBlacklist = new HashSet<GameObject>();
+
+            Material blackMaterial = new Material(Shader.Find("HDRP/Lit"));
+            blackMaterial.color = Color.black;
+            blackMaterial.SetFloat(surfaceTypeID, 0);
+            blackMaterial.DisableKeyword(emissiveColorMapKeyword);
+
+            static bool FastContains(string path, string keyword) =>
+                path.IndexOf(keyword, StringComparison.Ordinal) >= 0;
+
+            foreach (Light light in allLights)
             {
-                Light[] childLights = obj.GetComponentsInChildren<Light>(true);
-                foreach (Light light in childLights)
+                if (extraLogs)
+                    ScienceBirdTweaks.Logger.LogDebug($"Found light {light.name} in {GetObjectPath(light.transform.parent.gameObject)} with type {light.type} and bake type {light.bakingOutput.lightmapBakeType}");
+
+                Transform? parent = light.transform.parent;
+
+                if (parent == null)
+                    continue;
+
+                if (lightObjectsBlacklist.Contains(parent.gameObject))
+                    continue;                    
+
+                if (light.bakingOutput.lightmapBakeType == LightmapBakeType.Baked || parent.name == "BlackoutIgnore" || HasParentWithInteractTrigger(light.gameObject))
                 {
-                    if (light.bakingOutput.lightmapBakeType == LightmapBakeType.Baked)
-                    {
-                        if (light.transform != null && light.transform.parent != null)
-                        {
-                            lightObjectBlacklist.Add(light.transform.parent.gameObject);
-                        }
-                        continue;
-                    }
-                    GameObject? parent = null;
-                    if (light.transform != null)
-                    {
-                        if (light.transform.parent != null)
-                        {
-                            parent = light.transform.parent.gameObject;
-                        }
-                        else
-                        {
-                            ScienceBirdTweaks.Logger.LogDebug("Null parent!");
-                        }
-                    }
-                    else
-                    {
-                        ScienceBirdTweaks.Logger.LogDebug("Null transform!");
-                    }
-                    if (parent != null && !lightObjects.Contains(parent) && parent.name != "BlackoutIgnore")
-                    {
-                        lightObjects.Add(parent);
-                    }
-                    else if (parent == null)
-                    {
-                        ScienceBirdTweaks.Logger.LogDebug("Couldn't find parent! Skipping...");
-                    }
+                    lightObjectsBlacklist.Add(parent.gameObject);
+
+                    if (lightObjects.Contains(parent.gameObject))
+                        lightObjects.Remove(parent.gameObject);
+
+                    continue;
                 }
+
+                string path = GetObjectPath(parent.gameObject);
+
+                // TODO: pull these from a config file
+                if (FastContains(path, "HangarShip") ||
+                    FastContains(path, "PlayersContainer") ||
+                    FastContains(path, "MaskMesh") ||
+                    FastContains(path, "EyesFilled") ||
+                    FastContains(path, "GunBody") ||
+                    FastContains(path, "Landmine") ||
+                    FastContains(path, "Trap") ||
+                    (FastContains(path, "Door") && !FastContains(path, "FireExit")) ||
+                    //FastContains(parent.name, "Entrance") ||
+                    FastContains(parent.name, "Apparatus"))
+                    continue;
+
+                lightObjects.Add(parent.gameObject);
             }
+
+            ScienceBirdTweaks.Logger.LogDebug($"[TIMER] Light processing: {totalStopwatch.ElapsedMilliseconds}ms");
+            ScienceBirdTweaks.Logger.LogDebug($"Found {lightObjects.Count} light objects to process");
+
             if (lightObjects.Count() == 0)
             {
                 ScienceBirdTweaks.Logger.LogInfo("Found no lights!");
+                totalStopwatch.Stop();
+                ScienceBirdTweaks.Logger.LogDebug($"[TIMER] TOTAL BlackoutOverride execution time (no lights found): {totalStopwatch.ElapsedMilliseconds}ms");
                 return;
             }
-            ScienceBirdTweaks.Logger.LogDebug($"Done lights loop! Found {lightObjects.Count()} light objects ({lightObjectBlacklist.Count()} blacklisted).");
+
+            Color zeroEmission = new Color(0f, 0f, 0f, 1f);
+            Color black = new Color(0f, 0f, 0f, 1f);
 
             foreach (GameObject lightObject in lightObjects)
             {
-                string hierarchyString = string.Join("/", lightObject.GetComponentsInParent<Transform>().Select(t => t.name).Reverse().ToArray());
-                if (lightObjectBlacklist.Contains(lightObject) || hierarchyString.Contains("HangarShip") || hierarchyString.Contains("PlayersContainer") || hierarchyString.Contains("MaskMesh") || hierarchyString.Contains("EyesFilled") || hierarchyString.Contains("Systems/"))
-                {
-                    continue;
-                }
-                else if (hierarchyString.IsNullOrWhiteSpace())
-                {
-                    continue;
-                }
-                Renderer[] lightRenderers = lightObject.GetComponentsInChildren<Renderer>();
+                foreach (Light light in lightObject.GetComponentsInChildren<Light>(true))
+                    light.enabled = false;
+
+                if (extraLogs)
+                    ScienceBirdTweaks.Logger.LogDebug($"Disabling light object {lightObject.name} with path {GetObjectPath(lightObject)}");
+
+                Renderer[] lightRenderers = lightObject.GetComponentsInChildren<Renderer>(true);
+
                 foreach (Renderer renderer in lightRenderers)
                 {
-                    if (renderer.gameObject != lightObject)
+                    Material[] materials = renderer.materials;
+                    bool materialsModified = false;
+
+                    for (int i = 0; i < materials.Length; i++)
                     {
-                        hierarchyString = string.Join("/", renderer.gameObject.GetComponentsInParent<Transform>().Select(t => t.name).Reverse().ToArray());
-                    }
-                    Material[] rMaterials = renderer.materials;
-                    for (int i = 0; i < rMaterials.Length; i++)
-                    {
-                        if (rMaterials[i] != null)
+                        Material mat = materials[i];
+                        if (mat == null) continue;
+
+                        bool hasEmissionMap = mat.HasProperty(emissionMapID);
+                        bool hasUseEmissiveIntensity = mat.HasProperty(useEmissiveIntensityID);
+                        float emissiveIntensity = hasUseEmissiveIntensity ? mat.GetFloat(useEmissiveIntensityID) : 0f;
+                        bool isEmissiveKeywordEnabled = mat.IsKeywordEnabled(emissiveColorMapKeyword);
+
+                        bool hasEmission = hasEmissionMap || (hasUseEmissiveIntensity && emissiveIntensity > 0) || isEmissiveKeywordEnabled;
+
+                        if (hasEmission)
                         {
-                            if (renderer.materials[i].HasProperty("_EmissionMap") || renderer.materials[i].HasProperty("_UseEmissiveIntensity") && renderer.materials[i].GetFloat("_UseEmissiveIntensity") == 1f || renderer.materials[i].IsKeywordEnabled("_EMISSIVE_COLOR_MAP"))
-                            {
-                                if (!hierarchyString.IsNullOrWhiteSpace())
-                                {
-                                    GameObject targetObj = GameObject.Find(hierarchyString);
-                                    if (targetObj != null)
-                                    {
-                                        //ScienceBirdTweaks.Logger.LogDebug($"Darkening the material {rMaterials[i].name} of {renderer.gameObject.name} ({hierarchyString}).");
-                                        rMaterials[i].SetColor("_EmissionColor", new Color(0f, 0f, 0f, 1f));
-                                        rMaterials[i].SetColor("_EmissiveColor", new Color(0f, 0f, 0f, 1f));
-                                    }
-                                    else
-                                    {
-                                        ScienceBirdTweaks.Logger.LogDebug($"Couldn't find object with string {hierarchyString}!");
-                                    }
-                                }
-                                else
-                                {
-                                    ScienceBirdTweaks.Logger.LogDebug($"Null hierarchyString! Name: {renderer.gameObject.name}");
-                                }
-                            }
+                            if (mat.HasProperty(emissionColorID))
+                                mat.SetColor(emissionColorID, zeroEmission);
+
+                            if (mat.HasProperty(emissiveColorID))
+                                mat.SetColor(emissiveColorID, zeroEmission);
+
+                            if (hasUseEmissiveIntensity)
+                                mat.SetFloat(useEmissiveIntensityID, 0);
+
+                            materialsModified = true;
+
+                            if (extraLogs)
+                                ScienceBirdTweaks.Logger.LogDebug($"Darkened material {mat.name} of {renderer.gameObject.name}");
                         }
                         else
                         {
-                            ScienceBirdTweaks.Logger.LogDebug("Null material!");
+                            if (extraLogs)
+                                ScienceBirdTweaks.Logger.LogDebug($"Skipping material {mat.name} of {renderer.gameObject.name}");
+
+                            if (mat.name == "SkyEmissive (Instance)" || mat.name.Contains("LEDLight (Instance)")) // Probably look for if an HDRP/lit material has emission / is above a certain brightness
+                            {
+                                if (extraLogs)
+                                    ScienceBirdTweaks.Logger.LogDebug($"Found LED light material {mat.name} of {renderer.gameObject.name}");
+
+                                materials[i] = blackMaterial;
+                                materialsModified = true;
+                            }
                         }
                     }
-                    renderer.materials = rMaterials;
+
+                    if (materialsModified)
+                        renderer.materials = materials;
                 }
             }
+
+            RoundManager.Instance.SwitchPower(on: true);
+            RoundManager.Instance.TurnOnAllLights(on: false);
+            BreakerBox currentBreakerBox = UnityEngine.Object.FindObjectOfType<BreakerBox>();
+            if (currentBreakerBox != null)
+                currentBreakerBox.gameObject.SetActive(false);
+
+            UnityEngine.GameObject sun = UnityEngine.GameObject.Find("Sun");
+            if (sun != null)
+                sun.SetActive(false);
+
+            totalStopwatch.Stop();
+            ScienceBirdTweaks.Logger.LogDebug($"[TIMER] TOTAL BlackoutOverride execution time: {totalStopwatch.ElapsedMilliseconds}ms");
         }
 
-        public static void OnSetWeathers(Weather[] weathers)
+        private static string GetObjectPath(GameObject obj)
         {
-            bool foundBlackout = false;
-            foreach (Weather weather in weathers)
+            StringBuilder path = new StringBuilder(obj.name);
+            Transform current = obj.transform.parent;
+
+            while (current != null)
             {
-                if (weather != null)
-                {
-                    if (weather.Name == "Blackout")
-                    {
-                        foundBlackout = true;
-                        break;
-                    }
-                }
+                path.Insert(0, current.name + "/");
+                current = current.parent;
             }
-            if (foundBlackout)
-            {
-                BlackoutOverride();
-            }
+
+            return path.ToString();
         }
 
-        public static void OnSetWeatherTypes(LevelWeatherType[] weatherTypes)
+        private static bool HasParentWithInteractTrigger(GameObject obj)
         {
-            bool foundBlackout = false;
-            foreach (LevelWeatherType weatherType in weatherTypes)
-            {
-                Weather weather = WeatherManager.GetWeather(weatherType);
-                if (weather != null)
-                {
-                    if (weather.Name == "Blackout")
-                    {
-                        foundBlackout = true;
-                        break;
-                    }
-                }
-            }
-            if (foundBlackout)
-            {
-                BlackoutOverride();
-            }
-        }
+            Transform current = obj.transform;
 
-        public static void OnSetWeather(Weather weather)
-        {
-            if (weather != null)
+            while (current != null)
             {
-                if (weather.Name == "Blackout")
+                if (current.GetComponent<InteractTrigger>() != null)
                 {
-                    BlackoutOverride();
+                    ScienceBirdTweaks.Logger.LogDebug($"Found InteractTrigger in {current.name} with path {GetObjectPath(current.gameObject)}");
+                    return true;
                 }
+                   
+                current = current.parent;
             }
+
+            return false;
         }
     }
 }
