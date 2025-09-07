2ee1bfb6eb1a38f47e5084a23c6723b0e9983883
Science-Bird <80128986+Science-Bird@users.noreply.github.com>
2025-09-07 14:51:42 -0700

    Safer starting moon patch and fixed regex oopsie

--- Patches/StartingMoonPatch.cs
+++ Patches/StartingMoonPatch.cs
@@ -11,13 +11,16 @@ namespace ScienceBirdTweaks.Patches
         [HarmonyPrefix]
         static void OnStart(StartOfRound __instance)
         {
-            string moon = Regex.Replace(ScienceBirdTweaks.StartingMoon.Value, "^.\\d+\\s", "");// this regex removes numbers from the start (with check for additional character to catch Hyx)
+            string moon = Regex.Replace(ScienceBirdTweaks.StartingMoon.Value, "^.?\\d+\\s", "");// this regex removes numbers from the start (with check for additional character to catch Hyx)
             if (moon == "Experimentation" || moon == "") { return; }
-            SelectableLevel newStartLevel = UnityEngine.Resources.FindObjectsOfTypeAll<SelectableLevel>().Where(x => Regex.Replace(x.PlanetName, "^.\\d+\\s", "") == moon).First();
-            if (newStartLevel != null)
-            {
-                ScienceBirdTweaks.Logger.LogDebug($"New start level ID: {newStartLevel.levelID}, new start level name: {newStartLevel.PlanetName}");
-                __instance.defaultPlanet = newStartLevel.levelID;
+            SelectableLevel[] levelPool = UnityEngine.Resources.FindObjectsOfTypeAll<SelectableLevel>().Where(x => Regex.Replace(x.PlanetName, "^.?\\d+\\s", "") == moon).ToArray();
+            if (levelPool.Length > 0) {
+                SelectableLevel newStartLevel = levelPool.First();
+                if (newStartLevel != null)
+                {
+                    ScienceBirdTweaks.Logger.LogDebug($"New start level ID: {newStartLevel.levelID}, new start level name: {newStartLevel.PlanetName}");
+                    __instance.defaultPlanet = newStartLevel.levelID;
+                }
             }
         }
     }
